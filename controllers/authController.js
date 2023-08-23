46e826958d159ca24e7dbbaa3688720ed0960c1d
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-23 23:22:56 +0530

    backend updated login api, JWT token, validate hashedpassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,34 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
 export const register = async (req, res) => {
   const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
 
   const user = await User.create(req.body);
-  res.status(StatusCodes.OK).json({msg:"user created"})
+  res.status(StatusCodes.OK).json({ msg: "user created" });
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
+  if (!isValidUser) {
+    res.status(StatusCodes.NOT_FOUND).json({ msg: "Invalid Credentials" });
+  }
+
+  const token = createJWT({ userId: user._id });
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+
+  res.status(StatusCodes.CREATED).json({ msg: "user logged in" });
+};
 export const logout = async (req, res) => {};

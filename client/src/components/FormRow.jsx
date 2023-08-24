7cffcf932284541405f73fe44806e1ed3fde96ed
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-24 21:25:50 +0530

    frontend updated register page css

--- client/src/components/FormRow.jsx
+++ client/src/components/FormRow.jsx
@@ -1,15 +1,17 @@
-const FormRow = ({ type, name, labelText,  onChange }) => {
+const FormRow = ({ type, name, labelText, onChange }) => {
   return (
     <div>
-      <label htmlFor={name} >
+      <label htmlFor={name} className="font-bold text-xl">
         {labelText || name}
       </label>
+      <br />
       <input
         type={type}
         id={name}
         name={name}
         onChange={onChange}
         required
+        className="border border-gray-600 rounded w-64 py-2 px-3 mt-1"
       />
     </div>
   );
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,8 @@
-import { Form, redirect, Link } from "react-router-dom";
-import {FormRow} from '../components'
+import { Form,redirect, Link } from "react-router-dom";
+import { FormRow } from "../components";
 import customFetch from "../utils/customFetch";
 import { toast } from "react-toastify";
+import { HomeIcon } from "../assets/icons";
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
@@ -12,28 +13,37 @@ export const action = async ({ request }) => {
     return redirect("/login");
   } catch (error) {
     toast.error(error?.response?.data?.msg);
-
     return error;
   }
 };
 
 const Register = () => {
   return (
-    <div>
-      <Form method="post" className="form">
-       
-        <h4>Register</h4>
-        <FormRow type="text" name="name" />
-        <FormRow type="text" name="email" />
-        <FormRow type="text" name="password" />
-       <button type="submit">Register</button>
-        <p>
-          Already a member?
-          <Link to="/login">
-            Login
-          </Link>
-        </p>
-      </Form>
+    <div className="min-h-screen  flex items-center justify-center bg-gray-100">
+      <div className="max-w-2xl p-7 border bg-white rounded-lg shadow-lg">
+        <div className="flex items-center justify-center">
+          <HomeIcon className="w-12 h-12 mx-auto" />
+          <h4 className="text-2xl font-bold mx-1 ">Register Page</h4>
+        </div>
+        <Form method="post" className="mt-6">
+          <FormRow type="text" name="name" labelText="Name:" />
+          <FormRow type="text" name="email" labelText="Email:" />
+          <FormRow type="password" name="password" labelText="Password:" />
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 hover:bg-blue-600 text-white py-3 px-6 mt-6 rounded-lg"
+          >
+            Register
+          </button>
+          <p className="mt-4 text-center">
+            Already a member?{" "}
+            <Link to="/login" className="text-blue-500">
+              Login
+            </Link>
+          </p>
+        </Form>
+      </div>
     </div>
   );
 };

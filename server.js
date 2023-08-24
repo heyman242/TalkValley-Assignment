bc39e254b3a4de77e9deacf4e532ad29d1c1a4f4
Himanshu Bobade <himanshubobade96@gmail.com>
2023-08-24 23:26:42 +0530

    backend production ready

--- server.js
+++ server.js
@@ -11,15 +11,27 @@ import authRouter from "./routes/authRouter.js";
 import userRouter from "./routes/userRouter.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "./public")));
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", authenticateUser, userRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
 });
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
